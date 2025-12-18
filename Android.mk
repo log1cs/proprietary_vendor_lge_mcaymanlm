f94455a42436ea18d682924d975b95173e5598b1
Tuan Anh <tuan73176@gmail.com>
2025-12-18 03:25:17 +0000

    mcaymanlm-user 12 SKQ1.211103.001 6885120230801 release-keys
    Setup LFS while at it.
    
    Change-Id: I093135a593c89d0c729da131f3f35cfcab637b07

---
+++ Android.mk
@@ -0,0 +1,19 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),mcaymanlm)
+
+$(call add-radio-file-sha1-checked,radio/gz.img,282cbcc7a8be5fa056680ca3ad7eb9932f468e86)
+$(call add-radio-file-sha1-checked,radio/lk.img,ba32c2c13370d6d84f1345db2645721300868099)
+$(call add-radio-file-sha1-checked,radio/logo.img,25be75319a9009b86018a4206e9b4dfab62f2431)
+$(call add-radio-file-sha1-checked,radio/md1img.img,b977613b2f59a8748d1e6e5a3d8bf1c0d0f71606)
+$(call add-radio-file-sha1-checked,radio/preloader.img,c601fb14a7a4af6b410adea06c8dbcbf6e6a15fa)
+$(call add-radio-file-sha1-checked,radio/scp.img,b0ada531a47c816e007cfa9a40f9ea2219dc165a)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,7fc29d056b4b0daf44aff84f7c76a96baa5b5ef6)
+$(call add-radio-file-sha1-checked,radio/sspm.img,6cead0a473342837e41ae78d2c7ce4a92031c3e1)
+$(call add-radio-file-sha1-checked,radio/tee.img,ab0358a00622e4eb5bae29469259a723311855b6)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    gz \
+    lk \
+    logo \
+    md1img \
+    preloader \
+    scp \
+    spmfw \
+    sspm \
+    tee
